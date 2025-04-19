d3a716618a7a2d3834b787dfe702c6c4f963f44f
mahmoud abdelsalam <mahmoudabdelsalam144@gmail.com>
2025-04-19 02:48:25 +0200

    panel bar

--- MyForm.h
+++ MyForm.h
@@ -34,6 +34,23 @@ namespace SuperMarkoGUI {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Panel^ pn_left_bar;
+	protected:
+	private: System::Windows::Forms::Panel^ pn_upper_bar;
+	private: System::Windows::Forms::Panel^ pn_object_bar;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Panel^ panel1;
+
+
+
+	protected:
+
+	protected:
+
+	protected:
+
+
 
 	private:
 		/// <summary>
@@ -48,15 +65,94 @@ namespace SuperMarkoGUI {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->pn_left_bar = (gcnew System::Windows::Forms::Panel());
+			this->pn_upper_bar = (gcnew System::Windows::Forms::Panel());
+			this->pn_object_bar = (gcnew System::Windows::Forms::Panel());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->flowLayoutPanel1 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->pn_left_bar->SuspendLayout();
+			this->flowLayoutPanel1->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// pn_left_bar
+			// 
+			this->pn_left_bar->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(51)), static_cast<System::Int32>(static_cast<System::Byte>(55)),
+				static_cast<System::Int32>(static_cast<System::Byte>(69)));
+			this->pn_left_bar->Controls->Add(this->flowLayoutPanel1);
+			this->pn_left_bar->Controls->Add(this->panel1);
+			this->pn_left_bar->Dock = System::Windows::Forms::DockStyle::Left;
+			this->pn_left_bar->Location = System::Drawing::Point(0, 0);
+			this->pn_left_bar->Name = L"pn_left_bar";
+			this->pn_left_bar->Size = System::Drawing::Size(215, 797);
+			this->pn_left_bar->TabIndex = 0;
+			// 
+			// pn_upper_bar
+			// 
+			this->pn_upper_bar->Dock = System::Windows::Forms::DockStyle::Top;
+			this->pn_upper_bar->Location = System::Drawing::Point(215, 0);
+			this->pn_upper_bar->Name = L"pn_upper_bar";
+			this->pn_upper_bar->Size = System::Drawing::Size(892, 100);
+			this->pn_upper_bar->TabIndex = 1;
+			// 
+			// pn_object_bar
+			// 
+			this->pn_object_bar->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->pn_object_bar->Location = System::Drawing::Point(215, 100);
+			this->pn_object_bar->Name = L"pn_object_bar";
+			this->pn_object_bar->Size = System::Drawing::Size(892, 697);
+			this->pn_object_bar->TabIndex = 2;
+			// 
+			// panel1
+			// 
+			this->panel1->Dock = System::Windows::Forms::DockStyle::Top;
+			this->panel1->Location = System::Drawing::Point(0, 0);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(215, 169);
+			this->panel1->TabIndex = 0;
+			// 
+			// flowLayoutPanel1
+			// 
+			this->flowLayoutPanel1->Controls->Add(this->button1);
+			this->flowLayoutPanel1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->flowLayoutPanel1->Location = System::Drawing::Point(0, 169);
+			this->flowLayoutPanel1->Name = L"flowLayoutPanel1";
+			this->flowLayoutPanel1->Size = System::Drawing::Size(215, 628);
+			this->flowLayoutPanel1->TabIndex = 1;
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(51)), static_cast<System::Int32>(static_cast<System::Byte>(55)),
+				static_cast<System::Int32>(static_cast<System::Byte>(69)));
+			this->button1->CausesValidation = false;
+			this->button1->FlatAppearance->BorderSize = 0;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Segoe UI", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->ForeColor = System::Drawing::Color::White;
+			this->button1->Location = System::Drawing::Point(3, 3);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(212, 95);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Edit Information";
+			this->button1->UseVisualStyleBackColor = false;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(699, 428);
+			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(239)), static_cast<System::Int32>(static_cast<System::Byte>(239)),
+				static_cast<System::Int32>(static_cast<System::Byte>(230)));
+			this->ClientSize = System::Drawing::Size(1107, 797);
+			this->Controls->Add(this->pn_object_bar);
+			this->Controls->Add(this->pn_upper_bar);
+			this->Controls->Add(this->pn_left_bar);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"Super Marko";
+			this->pn_left_bar->ResumeLayout(false);
+			this->flowLayoutPanel1->ResumeLayout(false);
 			this->ResumeLayout(false);
 
 		}
